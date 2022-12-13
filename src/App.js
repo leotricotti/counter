caa854f3ccfa2fd4b033795746f3cbe44d575453
leotricotti <tricottileo@gmail.com>
2022-12-13 19:57:42 -0300

    master

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import "./App.css";
 
+function AddBtn() {
+  return <h1>AddBtn</h1>;
+}
+
+function MinusBtn() {
+  return <h1>MinusBtn</h1>;
+}
+
+function Counter() {
+  return <h1>Counter</h1>;
+}
+
+function AppContainer() {
+  return (
+    <>
+      <Counter />
+      <AddBtn />
+      <MinusBtn />
+    </>
+  );
+}
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <AppContainer />
+    </div>
+  );
 }
 
 export default App;
